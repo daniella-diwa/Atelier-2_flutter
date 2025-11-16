19168bfa14515adb0d3b48e29777074b0b3ebc47
Daniella Kembo <kembodaniella@gmail>
2025-11-15 19:19:22 -0500

    Etape 5

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,16 @@ class MainApp extends StatelessWidget {
                   ),
                 ],
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.home, color: Colors.blue, size: 40),
+                  Icon(Icons.star, color: Colors.amber, size: 40),
+                  Icon(Icons.settings, color: Colors.grey, size: 40),
+                  Icon(Icons.person, color: Colors.green, size: 40),
+                ],
+              ),
             ],
           ),
         ),
